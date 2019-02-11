c67fcc74fcce9660ceaa28a8f96c6ba96104b22f
Mashroor Rashid <mashroor@sbcglobal.net>
2019-02-11 15:44:14 -0600

    merge to master

--- cs3800Proj1_directory.h
+++ cs3800Proj1_directory.h
@@ -29,6 +29,7 @@ class directory{
         directory(): parent(nullptr) {}; //used in mkdir(), to allocate a new directory obj
         directory(string name, directory &newParent); //used for any time a diretory is made
         ~directory(); //since making a new directory, need a way to deref, avoid mem leaking
+        
         //getters, since private
         string getPermissions() const{return permissions;}
         string getUserName() const{return userName;}
@@ -45,15 +46,44 @@ class directory{
         void setTimestamp();
         void setName(string name){directoryName = name;}
         void setPermissions(string permCode);
+
         //start directory commands
+
+        //Func: cd(string, directory*)
+        //Pre;
+        //Post:
         directory* cd(string objName, directory* newParent);
-        void pwd();
+        //Func: pwd()
+        //Pre;
+        //Post:
+        void pwd();        
+        //Func: pwd()
+        //Pre;
+        //Post:        
         void ls();
+        //Func: ls()
+        //Pre;
+        //Post:  
         void ls_l();
+        //Func: chmod (string0, string1)
+        //Pre;
+        //Post:  
         void chmod(string permCode, string dirName);
+        //Func:mkdir(string)
+        //Pre;
+        //Post:  
         void mkdir(string newDirName);
+        //Func: touch(string)
+        //Pre;
+        //Post:  
         void touch(string newFileName);
+        //Func: rmdir(string)
+        //Pre;
+        //Post:  
         void rmdir(string dirToDel);
+        //Func: rm(string)
+        //Pre;
+        //Post:  
         void rm(string fileToDel);
 
 };
